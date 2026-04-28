85bc0b91dd6d15430022d28aa7dc79487066ba8d
mariadutrasenai-lang <mariadutrasenai@gmail.com>
2026-04-28 11:26:58 -0300

    Add files via upload

---
+++ projetos-programacao/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atv2
+{
+    internal class Pessoa
+    {
+        public string nome;
+        public int idade;
+
+    }
+}
--- projetos-programacao/Program.cs
+++ projetos-programacao/Program.cs
@@ -4,29 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace atv2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Funcionario f1 = new Funcionario();
-            Funcionario f2 = new Funcionario();
+            Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa();
 
-            Console.WriteLine("Dados do primeiro funcionário: ");
-            Console.WriteLine("Nome: ");
-            f1.nome=Console.ReadLine();
-            Console.WriteLine("salário");
-            f1.salario = double.Parse(Console.ReadLine());
+            Console.WriteLine("dados da primeira pessoa: ");
+            Console.WriteLine("nome");
+            p1.nome = Console.ReadLine();
+            Console.WriteLine("idade: ");
+            p1.idade = int.Parse(Console.ReadLine());
 
+            Console.WriteLine("dados da segunda pessoa: ");
+            Console.WriteLine("nome");
+            p2.nome = Console.ReadLine();
+            Console.WriteLine("idade: ");
+            p2.idade = int.Parse(Console.ReadLine());
 
-
-            Console.WriteLine("Dados do primeiro funcionário: ");
-            Console.WriteLine("Nome: ");
-            f2.nome = Console.ReadLine();
-            Console.WriteLine("salario");
-            f2.salario = double.Parse(Console.ReadLine());
+            if (p1.idade > p2.idade)
+            {
+                Console.WriteLine("primeira pessoa mais velha");
+            }
+            else
+            {
+                Console.WriteLine("segunda pessoa mais velha");
+            }
         }
     }
 }
- 
