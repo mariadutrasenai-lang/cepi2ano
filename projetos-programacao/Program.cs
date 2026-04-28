41f55db52f0f1f94c3e9b7f0210135792da73112
mariadutrasenai-lang <mariadutrasenai@gmail.com>
2026-04-28 11:39:59 -0300

    Add files via upload

--- projetos-programacao/Program.cs
+++ projetos-programacao/Program.cs
@@ -4,35 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace atv2
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Pessoa p1 = new Pessoa();
-            Pessoa p2 = new Pessoa();
+            Funcionario f1 = new Funcionario();
+            Funcionario f2 = new Funcionario();
 
-            Console.WriteLine("dados da primeira pessoa: ");
-            Console.WriteLine("nome");
-            p1.nome = Console.ReadLine();
-            Console.WriteLine("idade: ");
-            p1.idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Dados do primeiro funcionário: ");
+            Console.WriteLine("Nome: ");
+            f1.nome=Console.ReadLine();
+            Console.WriteLine("salário");
+            f1.salario = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("dados da segunda pessoa: ");
-            Console.WriteLine("nome");
-            p2.nome = Console.ReadLine();
-            Console.WriteLine("idade: ");
-            p2.idade = int.Parse(Console.ReadLine());
 
-            if (p1.idade > p2.idade)
-            {
-                Console.WriteLine("primeira pessoa mais velha");
-            }
-            else
-            {
-                Console.WriteLine("segunda pessoa mais velha");
-            }
+
+            Console.WriteLine("Dados do segundo funcionário: ");
+            Console.WriteLine("Nome: ");
+            f2.nome = Console.ReadLine();
+            Console.WriteLine("salario: ");
+            f2.salario = double.Parse(Console.ReadLine());
+            
+            double salariomedia = (f1.salario + f2.salario) / 2.0;
+            Console.WriteLine("salário medio = ");
+
+            Console.WriteLine("primeiro funcionario: " + f1.nome);
+            Console.WriteLine("segundo funcionario: " + f2.nome);
         }
     }
 }
+ 
