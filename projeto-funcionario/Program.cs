3098b0a9fc3c780322dc573a13d25d80fae6246b
mariadutrasenai-lang <mariadutrasenai@gmail.com>
2026-05-05 10:24:52 -0300

    Add files via upload

---
+++ projeto-funcionario/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace mariiidutra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario func = new Funcionario();
+
+            Console.WriteLine("Nome: ");
+            func.Nome = Console.ReadLine();
+            Console.WriteLine("Salario Bruto: ");
+            func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Imposto: ");
+            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Funcionario: " + func);
+
+            Console.Write("deseja aumentar o salario em qual porcentagem");
+            double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            func.AumentarSalario(porcent);
+            Console.WriteLine("");
+            Console.WriteLine("Dados atualizados: " + func);
+
+        }
+    }
+}
